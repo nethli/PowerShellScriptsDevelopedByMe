2debf5a6fcd989350b5d7fff2f3ba76053dbde4b
nethli <nethlidahanayake@gmail.com>
2025-07-27 09:08:18 +0530

    Get Average Using a Function

---
+++ functionAverage.ps1
@@ -0,0 +1,9 @@
+function Retrieve-Average{
+    param(
+        [int] $num1,
+        [int] $num2
+    )
+
+    $result = (($num1 + $num2)/2)
+    write-host "The average of the two numbers are $result"
+}
